835383eb31d3e8d145bbde9e9a3ea5df480b1dad
Nei Poeta <neipoeta@outlook.com>
2021-06-25 00:49:56 -0300

    firstCommit

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,116 @@
+import { useParams } from 'react-router'
+import logoImg from '../assets/images/logo.svg'
+import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
+import { Button } from '../components/Button'
+import { Question } from '../components/Question'
+import { RoomCode } from '../components/RoomCode'
+// import { useAuth } from '../hooks/useAuth'
+import { useRoom } from '../hooks/useRoom'
+
+
+import '../styles/room.scss'
+import { database } from '../services/firebase'
+import { useHistory } from 'react-router-dom'
+
+type RoomParams = {
+    id: string
+}
+
+export function AdminRoom() {
+    // const { user } = useAuth()
+    const history = useHistory()
+    const params = useParams<RoomParams>()
+    const roomId = params.id
+
+    const { title, questions } = useRoom(roomId)
+
+
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date(),
+        })
+
+        history.push('/')
+    }
+
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true
+        })
+    }
+
+    async function hendleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true,
+        })
+    }
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Tem certeza que você deseja excluir está pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+        }
+    }
+
+
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="LetMeAsk" />
+                    <div>
+                    <RoomCode code={roomId}/>
+                    <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
+                    </div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+                </div>
+
+               
+                <div className="question-list">
+                {questions.map(question => {
+                    return (
+                        <Question
+                            key={question.id}
+                            content={question.content}
+                            author={question.author}
+                            isAnswered={question.isAnswered}
+                            isHighlighted={question.isHighlighted}
+                        >
+                            {!question.isAnswered && (
+                                <>
+                                    <button
+                                type="button"
+                                onClick={() => handleCheckQuestionAsAnswered(question.id)}    
+                            >
+                                 <img src={checkImg} alt="Marcar pergunta como respondida"/>
+                             </button>
+                             <button
+                                type="button"
+                                onClick={() => hendleHighlightQuestion(question.id)}    
+                            >
+                                 <img src={answerImg} alt="Dar destaque à respondida"/>
+                             </button>
+                                </>
+                            )}
+                            <button
+                                type="button"
+                                onClick={() => handleDeleteQuestion(question.id)}    
+                            >
+                                 <img src={deleteImg} alt="Remover pergunta"/>
+                             </button>
+                        </Question>
+                    )
+                })}
+                </div>
+            </main>
+        </div>
+    )
+}
